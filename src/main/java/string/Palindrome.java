789ea6c6844afa1404d4a3a69f8e0c0a09e4715a
r-sreesaran <r.sreesaran@gmail.com>
2017-12-25 20:37:03 +0530

    added Test for Palindrome

--- src/main/java/string/Palindrome.java
+++ src/main/java/string/Palindrome.java
@@ -3,4 +3,22 @@ package main.java.string;
 
 public class Palindrome {
 
+    public boolean isPalindrome(String testString) {
+int i=0,j=testString.length()-1;
+
+while (i<j) {
+
+    while(Character.isLetterOrDigit(testString.charAt(i)  ) && i<j)
+    i++;
+
+    while(Character.isLetterOrDigit(testString.charAt(j)  ) && i<j)
+        j--;
+
+   if(Character.toLowerCase(testString.charAt(i++))!=Character.toLowerCase(testString.charAt(j--) )) {
+       return false;
+   }
+
+}
+        return true;
+    }
 }
--- src/test/java/TestPalindrome.java
+++ src/test/java/TestPalindrome.java
@@ -1,13 +1,21 @@
 package test.java;
 
+import main.java.string.Palindrome;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestPalindrome {
 
+    Palindrome instance;
+    @BeforeClass
+    public void intialize() {
+instance = new Palindrome();
+    }
+
     @Test
     public void TestPalindrome() {
-        Assert.assertTrue(true);
+        Assert.assertTrue(instance.isPalindrome("dad"));
 
     }
 
