6841a0a0e9b6d7c206845abf13e1e956f76e63e4
r-sreesaran <r.sreesaran@gmail.com>
2017-12-25 20:15:37 +0530

    first Commit

---
+++ src/main/java/string/Palindrome.java
@@ -0,0 +1,6 @@
+package main.java.string;
+
+
+public class Palindrome {
+
+}
---
+++ src/test/java/TestPalindrome.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestPalindrome {
+
+    @Test
+    public void TestPalindrome() {
+        Assert.assertTrue(true);
+
+    }
+
+}
