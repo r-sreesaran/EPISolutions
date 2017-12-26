5f830fcc3d3b63d7e486b679710b7a2e8426d598
r-sreesaran <r.sreesaran@gmail.com>
2017-12-26 08:47:58 +0530

    added Reverse all words

---
+++ src/main/java/string/ReverseAllWords.java
@@ -0,0 +1,22 @@
+package main.java.string;
+
+import java.util.Random;
+
+public class ReverseAllWords {
+    String[] values;
+
+    public void operation(String word) {
+
+        values = word.split(" ");
+        for(int i=values.length-1;i>=0;i--) {
+            System.out.println(values[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        ReverseAllWords test = new ReverseAllWords();
+        test.operation("a cat and dog");
+    }
+
+
+}
