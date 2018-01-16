53a7eb9de893af8e69cc24f26d71071dc3682ecd
r-sreesaran <r.sreesaran@gmail.com>
2018-01-16 23:44:50 +0530

    completed

--- src/main/java/string/RomanToDecimal.java
+++ src/main/java/string/RomanToDecimal.java
@@ -1,4 +1,34 @@
 package string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToDecimal {
+
+    Map <String, Integer> romanNumeral = new HashMap<String,Integer>(){
+        {
+            put("I",1);
+            put("V",5);
+            put("X",10);
+            put("L",50);
+            put("C",100);
+            put("D",500);
+            put("M",1000);
+        }
+    };
+
+    public int convert(String numeral) {
+        int sum = romanNumeral.get(numeral.charAt(numeral.length()-1));
+
+        for(int i = numeral.length()-1;i>0;i--) {
+            if(romanNumeral.get(numeral.charAt(i))>romanNumeral.get(numeral.charAt(i+1))) {
+                sum = sum - romanNumeral.get(numeral.charAt(i+1));
+            }
+            else {
+                sum = sum + romanNumeral.get(numeral.charAt(i+1));
+            }
+        }
+        return sum;
+    }
+
 }
