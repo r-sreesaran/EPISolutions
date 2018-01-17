5ab452b9063b9247f7fdfef1162a703757391163
r-sreesaran <r.sreesaran@gmail.com>
2018-01-17 08:58:31 +0530

    corrected the test

--- src/main/java/string/RomanToDecimal.java
+++ src/main/java/string/RomanToDecimal.java
@@ -5,27 +5,27 @@ import java.util.Map;
 
 public class RomanToDecimal {
 
-    Map <String, Integer> romanNumeral = new HashMap<String,Integer>(){
+    Map <Character, Integer> romanNumeral = new HashMap<Character, Integer>(){
         {
-            put("I",1);
-            put("V",5);
-            put("X",10);
-            put("L",50);
-            put("C",100);
-            put("D",500);
-            put("M",1000);
+            put('I',1);
+            put('V',5);
+            put('X',10);
+            put('L',50);
+            put('C',100);
+            put('D',500);
+            put('M',1000);
         }
     };
 
     public int convert(String numeral) {
         int sum = romanNumeral.get(numeral.charAt(numeral.length()-1));
 
-        for(int i = numeral.length()-1;i>0;i--) {
-            if(romanNumeral.get(numeral.charAt(i))>romanNumeral.get(numeral.charAt(i+1))) {
-                sum = sum - romanNumeral.get(numeral.charAt(i+1));
+        for(int i = numeral.length()-2;i>=0;i--) {
+            if(romanNumeral.get(numeral.charAt(i))<romanNumeral.get(numeral.charAt(i+1))) {
+                sum = sum - romanNumeral.get(numeral.charAt(i));
             }
             else {
-                sum = sum + romanNumeral.get(numeral.charAt(i+1));
+                sum = sum + romanNumeral.get(numeral.charAt(i));
             }
         }
         return sum;
