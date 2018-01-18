b2a4d034fa4a83410b2bcc1b24465b434b4e7990
r-sreesaran <r.sreesaran@gmail.com>
2018-01-18 09:59:07 +0530

    refractored code

--- src/main/java/string/ComputeValidIPAddress.java
+++ src/main/java/string/ComputeValidIPAddress.java
@@ -6,8 +6,12 @@ import java.util.stream.Stream;
 //http://elementsofprogramminginterviews.com/solutions/java/ValidIPAddress.java
 public class ComputeValidIPAddress {
 
+
     public void validateIPAddress(String ip) {
-      //  Stream.of(ip.split(".")).allMatch()
+        Stream.of(ip.split("."))
+                .filter(r -> r.length()>3 &&!r.startsWith("0"));
+
+
 
     }
 }
