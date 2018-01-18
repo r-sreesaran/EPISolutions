20f06891b04054bf8b749f243a2bb3c84af89b85
r-sreesaran <r.sreesaran@gmail.com>
2018-01-18 23:29:39 +0530

    added ipaddress validator

--- src/main/java/string/ComputeValidIPAddress.java
+++ src/main/java/string/ComputeValidIPAddress.java
@@ -1,17 +1,21 @@
 package string;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 //http://elementsofprogramminginterviews.com/solutions/java/ValidIPAddress.java
 public class ComputeValidIPAddress {
 
 
-    public void validateIPAddress(String ip) {
-        Stream.of(ip.split("."))
-                .filter(r -> r.length()>3 &&!r.startsWith("0"));
+    public int validateIPAddress(String ip) {
+       List<String> parts = Stream.of(ip.split("\\."))
+         .filter(s->s.length()>0&&!s.startsWith("0"))
+        .filter(s->Integer.valueOf(s)<=255&&Integer.valueOf(s)>=0)
+        .collect(Collectors.toList());
 
-
+return parts.size();
 
     }
 }
--- src/test/java/TestComputeValidIPAddress.java
+++ src/test/java/TestComputeValidIPAddress.java
@@ -1,2 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import string.ComputeValidIPAddress;
+
 public class TestComputeValidIPAddress {
+
+    ComputeValidIPAddress ipAddress;
+
+    @BeforeClass
+    public void setup() {
+        ipAddress = new ComputeValidIPAddress();
+    }
+
+    @Test
+    public void TestValidIp() {
+
+        Assert.assertEquals(ipAddress.validateIPAddress("192.168.1.1"),4);
+    }
 }
