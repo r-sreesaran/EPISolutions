71a0c5f5ab1597ec26005d9e10037f4f1124b468
r-sreesaran <r.sreesaran@gmail.com>
2018-01-20 22:32:00 +0530

    added solution for printing sinusoidally string

--- src/main/java/string/StringSinusoidally.java
+++ src/main/java/string/StringSinusoidally.java
@@ -3,4 +3,23 @@ package string;
 
 //http://elementsofprogramminginterviews.com/solutions/java/SnakeString.java
 public class StringSinusoidally {
+
+    public String printString(String input) {
+    String[] values = input.split("");
+String output=null;
+
+for(int i=1;i<values.length;i+=4) {
+output +=values[i];
+}
+
+        for(int i=0;i<values.length;i+=2) {
+            output +=values[i];
+        }
+        for(int i=3;i<values.length;i+=4) {
+            output +=values[i];
+        }
+
+
+return output;
+    }
 }
