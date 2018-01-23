92406d0b6ee9c14b23747478b381420c50ee5416
r-sreesaran <r.sreesaran@gmail.com>
2018-01-23 22:39:35 +0530

    added classes for Run Length Encoding

--- src/main/java/string/RunLengthEncoding.java
+++ src/main/java/string/RunLengthEncoding.java
@@ -1,5 +1,13 @@
 package string;
 
+import java.util.Arrays;
+
 public class RunLengthEncoding {
   //http://elementsofprogramminginterviews.com/solutions/java/RunLengthCompression.java
+
+    public String performEncoding(String values) {
+       // Arrays.stream(values.split("")).
+        return "";
+    }
 }
+
