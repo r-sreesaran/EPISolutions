59f8f9977c4fd91ca7773ad165cdd51426da191b
r-sreesaran <r.sreesaran@gmail.com>
2018-01-27 00:07:48 +0530

    added classes for Run Length Encoding

--- src/main/java/string/RunLengthEncoding.java
+++ src/main/java/string/RunLengthEncoding.java
@@ -1,13 +1,28 @@
 package string;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class RunLengthEncoding {
   //http://elementsofprogramminginterviews.com/solutions/java/RunLengthCompression.java
 
     public String performEncoding(String values) {
-       Arrays.stream(values.split(""));
-        return "";
+        /*
+        Take the first value
+        print the no of times the character is specified
+        * */
+        int cache=0;
+        String[] data = values.split("");
+        String result="";
+        for(int i=0;i<=data.length/2;i=i+2) {
+            for(int j=Integer.valueOf(data[i]);j>0;j--) {
+                result = result+data[i+1];
+            }
+        }
+        System.out.println(result);
+        return result;
     }
+
+
 }
 
