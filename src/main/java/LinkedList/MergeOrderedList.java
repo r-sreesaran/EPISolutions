7ce8eb5101ec5b5fa048917ccbe1fbba8f37a5cd
r-sreesaran <r.sreesaran@gmail.com>
2018-02-02 23:07:32 +0530

    added logic merging two linked list.

--- src/main/java/LinkedList/MergeOrderedList.java
+++ src/main/java/LinkedList/MergeOrderedList.java
@@ -10,7 +10,21 @@ ListNode<Integer> intial = new ListNode<>(0,null);
 ListNode<Integer> current = intial;
         ListNode<Integer> p1=L1, p2=L2;
 
-//while()
-        return null;
+while(p1==null && p2==null) {
+    if(p1.data<= p2.data) {
+        current.next =p2;
+        p1=p1.next;
+
+    }
+    else  {
+        current.next = p2;
+        p2 = p2.next;
+    }
+    current = current.next;
+}
+
+// Appends the remaining nodes of p1 or p2.
+        current.next = p1 != null ? p1 : p2;
+        return intial.next;
     }
 }
