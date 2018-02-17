6a7534dde909a682db32c5255683075b7125086a
r-sreesaran <r.sreesaran@gmail.com>
2018-02-17 23:48:04 +0530

    reverted code changes

--- src/test/java/LinkedList/TestReverseSubList.java
+++ src/test/java/LinkedList/TestReverseSubList.java
@@ -1,25 +1,11 @@
 package LinkedList;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.util.ArrayList;
+
 
 public class TestReverseSubList {
 
 
-        public void processData() {
 
-        }
-
-        public void readFile() {
-            File file = new File("//Users//Downloads//test_cases//input.txt");
-            ArrayList<String> al=new ArrayList<String>();//creating new generic arraylist
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String st;
-            while ((st = br.readLine()) != null)
-                al.add(st);
-            br.close();
-        }
     }
 
 
