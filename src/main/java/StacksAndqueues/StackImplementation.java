ff3a41c9a80ef46811493ca0ef2113977d366fe3
r-sreesaran <r.sreesaran@gmail.com>
2018-02-28 00:02:22 +0530

    added a additional step for test assertion

--- src/main/java/StacksAndqueues/StackImplementation.java
+++ src/main/java/StacksAndqueues/StackImplementation.java
@@ -1,4 +1,25 @@
 package StacksAndqueues;
 
+import java.util.LinkedList;
+
 public class StackImplementation {
+
+    LinkedList<Object> stack = new LinkedList();
+
+    public void StackImplementation(LinkedList data) {
+stack = data;
+    }
+
+    public void push() {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public void peek() {
+
+    }
+
 }
