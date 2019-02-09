be253162783a5b911d4a5b42838acff8fd3adee2
r-sreesaran <r.sreesaran@gmail.com>
2019-02-09 09:10:20 +0530

    added list of operations

--- src/test/java/LinkedList/TestLinkedList.java
+++ src/test/java/LinkedList/TestLinkedList.java
@@ -14,4 +14,16 @@ public class TestLinkedList {
         Assert.assertEquals(ll1.toString(),"1->2->3 End");
     }
 
+    @Test
+    public void mergeTwoNodes() {
+        ListNode<Integer> ll3 = new ListNode<Integer>(6,null);
+        ListNode<Integer> ll2 = new ListNode<Integer>(3,ll3);
+        ListNode<Integer> ll1 = new ListNode<Integer>(1,ll2);
+
+        ListNode<Integer> ll6 = new ListNode<Integer>(11,null);
+        ListNode<Integer> ll5 = new ListNode<Integer>(9,ll6);
+        ListNode<Integer> ll4 = new ListNode<Integer>(8,ll5);
+        
+    }
+
 }
